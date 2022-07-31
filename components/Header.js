5a5d807c27e21f92b9ee27061c9e5ba3d6aa7a52
Simon Browne <70622234+simmmmo@users.noreply.github.com>
2022-07-31 21:47:05 +1000

    feat/create home hero and update nav

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,10 @@
-/* This example requires Tailwind CSS v2.0+ */
 const navigation = [
-  { name: 'Solutions', href: '#' },
-  { name: 'Pricing', href: '#' },
-  { name: 'Docs', href: '#' },
-  { name: 'Company', href: '#' },
+  { name: 'Home', href: '/' },
+  { name: 'Explore', href: '#' },
+  { name: 'Create', href: 'create' },
+  { name: 'About', href: 'about' },
+  { name: 'Event Single', href: 'event' },
+  // { name: 'Add', href: 'new' },
 ]
 
 export default function Header() {
@@ -12,11 +13,11 @@ export default function Header() {
       <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" aria-label="Top">
         <div className="w-full py-6 flex items-center justify-between border-b border-indigo-500 lg:border-none">
           <div className="flex items-center">
-            <a href="#">
-              <span className="sr-only">Workflow</span>
+            <a href="/">
+              <span className="sr-only">WorkingBee</span>
               <img
                 className="h-10 w-auto"
-                src="https://tailwindui.com/img/logos/workflow-mark.svg?color=white"
+                src="https://s3.ap-southeast-2.amazonaws.com/testenv.com.au/bootcamp/working-bee/workingbee-logo.png"
                 alt=""
               />
             </a>
@@ -30,13 +31,13 @@ export default function Header() {
           </div>
           <div className="ml-10 space-x-4">
             <a
-              href="#"
+              href="signin"
               className="inline-block bg-indigo-500 py-2 px-4 border border-transparent rounded-md text-base font-medium text-white hover:bg-opacity-75"
             >
               Sign in
             </a>
             <a
-              href="#"
+              href="signup"
               className="inline-block bg-white py-2 px-4 border border-transparent rounded-md text-base font-medium text-indigo-600 hover:bg-indigo-50"
             >
               Sign up
